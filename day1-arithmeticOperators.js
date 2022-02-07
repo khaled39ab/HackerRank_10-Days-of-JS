04fe1b42b2b4553edf4300a5c577c015f9dd702b
khaled39ab <khaled39ab@gmail.com>
2022-02-07 20:55:14 +0600

    Day 1- ArithmeticOperators

---
+++ day1-arithmeticOperators.js
@@ -0,0 +1,49 @@
+// Task
+
+// Complete the following functions in the editor below:
+// 1- getArea(length, width): Calculate and return the area of a rectangle having sides length and width.
+// 2- getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides length and width.
+// The values returned by these functions are printed to stdout by locked stub code in the editor.
+
+// The area of the rectangle is (length * width ).
+// The perimeter of the rectangle is (2 * length + width)
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width)
+    return perimeter;
+}
+
+
