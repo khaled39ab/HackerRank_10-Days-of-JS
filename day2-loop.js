bb426ab4b2c033484c18cd94480eaa1c8774e7ea
khaled39ab <khaled39ab@gmail.com>
2022-10-03 23:57:02 +0600

    error in loops problm

---
+++ day2-loop.js
@@ -0,0 +1,16 @@
+/* 
+Task
+
+1-First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel must be printed in the same order as it appeared in .
+2-Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .
+Function Description
+
+Complete the vowelsAndConsonants function in the editor below.
+
+vowelsAndConsonants has the following parameters:
+
+string s: the string to process
+Prints
+
+Print each vowel of  in order on a new line, then print each consonant in order on a new line. Return nothing.
+ */
