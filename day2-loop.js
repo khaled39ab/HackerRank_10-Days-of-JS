8bcc7a55237a01940753e38c2228345022d5ba67
khaled39ab <khaled39ab@gmail.com>
2022-10-04 21:56:47 +0600

    solve loops problm

--- day2-loop.js
+++ day2-loop.js
@@ -14,3 +14,52 @@ Prints
 
 Print each vowel of  in order on a new line, then print each consonant in order on a new line. Return nothing.
  */
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+
+    for(let i of s){
+        if (vowels.includes(i)){
+            console.log(i);
+        }
+    }
+    for (let i of s){
+        if (!vowels.includes(i)){
+            console.log(i);
+        }
+    }
+}
+vowelsAndConsonants('javascriptloops')
+/* function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+} */
