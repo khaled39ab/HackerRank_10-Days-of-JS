c1056f8704b38f847ee51fc26e5129af8bbd5f4c
khaled39ab <khaled39ab@gmail.com>
2022-10-09 21:13:50 +0600

    formatting

--- day3-throw.js
+++ day3-throw.js
@@ -1,3 +1,45 @@
+// Task
+
+// Complete the isPositive function below. It has one integer parameter, a.If the value of a is positive, it must return the string YES.Otherwise, it must throw an Error according to the following rules:
+
+// * If a is 0, throw an Error with message = Zero Error.
+// * If a is negative, throw an Error with message = Negative Error.
+// Input Format
+
+// Locked stub code in the editor reads the following input from stdin and passes each value of a to the function as an argument:
+// The first line is an integer, n, denoting the number of times the function will be called with some a.
+// Each line i of the n subsequent lines contains an integer denoting some a.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
 function isPositive(a) {
     if (a > 0) {
         return 'YES'
@@ -7,4 +49,18 @@ function isPositive(a) {
         return 'Negative Error'
     }
 }
-console.log(isPositive(5));
+
+
+function main() {
+    const n = +(readLine());
+
+    for (let i = 0; i < n; i++) {
+        const a = +(readLine());
+
+        try {
+            console.log(isPositive(a));
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+}
--- day3-tryCatchFinally.js
+++ day3-tryCatchFinally.js
@@ -1,8 +1,47 @@
+// Task
+
+// Complete the reverseString function; it has one parameter, s.You must perform the following actions:
+
+// 1. Try to reverse string s using the split, reverse, and join methods.
+// 2. If an exception is thrown, catch it and print the contents of the exception's message on a new line.
+// 3. Print s on a new line.If no exception was thrown, then this should be the reversed string; if an exception was thrown, this should be the original string.
+// Input Format
+
+// Locked stub code in the editor reads variable s from stdin and passes it to the function.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
 function reverseString(s) {
     try {
         s = s.split('').reverse().join('');
     }
-    catch(e) {
+    catch (e) {
         console.log(e.message);
     }
     finally {
@@ -10,4 +49,9 @@ function reverseString(s) {
     }
 }
 
-reverseString(1230);
+
+function main() {
+    const s = eval(readLine());
+
+    reverseString(s);
+}
