6ab41eecb25fb2cf663ac17910d9bbec610e99cf
khaled39ab <khaled39ab@gmail.com>
2022-10-09 22:59:17 +0600

    suffering day4-count object

---
+++ day4-countObject.js
@@ -0,0 +1,61 @@
+// Task
+
+// Complete the function in the editor.It has one parameter: an array, a, of objects.Each object in the array has two integer properties denoted by x and y.The function must return a count of all such objects o in array a that satisfy o.x == o.y.
+
+// Input Format
+
+// The first line contains an integer denoting n.
+// Each of the n subsequent lines contains two space - separated integers describing the values of x and y.
+
+//     Constraints
+//     * 5 <= n <= 10
+//     * 1 <= x, y <= 100
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    // code here
+}
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
